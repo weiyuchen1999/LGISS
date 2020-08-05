cceff16ac93101da0e993bb9d35fa4af7dfc9c2c
weiyuchen1999 <66942825+weiyuchen1999@users.noreply.github.com>
2020-08-05 22:18:26 +0800

    Add files via upload

---
+++ LazyHVSelection.m
@@ -0,0 +1,92 @@
+function [Subset] = LazyHVSelection(PopObj)
+    a = size(PopObj, 1);
+    M = size(PopObj, 2);
+    selectedPop = [];
+    
+    heap.size=0;
+    heap.index=[];
+    heap.hvc=[];
+    
+    for n = 1 : a
+       heap.index(n) = n; 
+       currentPop = [selectedPop; PopObj(n, :)];
+       heap.hvc(n) = HVC(currentPop, max(PopObj,[],1)*1.1, size(currentPop, 1));
+       heap.size = heap.size + 1;       
+    end
+       
+    si = heap.size;
+    while(si >= 1)
+        heap = heap_down_sink(si, heap);
+        si = si - 1;
+    end
+	
+    %% Select first solution
+    top = heap.index(1);
+    selectedPop = [selectedPop; PopObj(top, :)];
+    heap = min_heap_popup(heap);
+	
+    %% Select other solutions
+    while size(selectedPop, 1) < 100
+        while true
+            last_index = heap.index(1);
+            top = heap.index(1);
+            cuurentPop = [selectedPop; PopObj(top, :)];
+            heap.hvc(1) = HVC(cuurentPop, max(PopObj,[],1)*1.1, size(cuurentPop, 1));
+            heap = heap_down_sink(1, heap);
+            
+			%% If the re-evaluated HVC is still the largest
+            if(heap.index(1) == last_index)            
+                selectedPop = [selectedPop; PopObj(heap.index(1), :)];
+                heap = min_heap_popup(heap);
+                break;  
+            end
+                
+        end   
+        
+    end
+	
+    %% output 
+    Subset = selectedPop;   
+  
+end
+
+function heap = heap_down_sink(self, heap)
+lchild =self*2;
+rchild =self*2+1;
+if(lchild <= heap.size && rchild <= heap.size)
+    if(heap.hvc(self) < heap.hvc(lchild) || heap.hvc(self) < heap.hvc(rchild))
+        if(heap.hvc(lchild) >= heap.hvc(rchild))
+            
+            heap = heap_swap_node(lchild,self,heap);
+            heap = heap_down_sink(lchild, heap);
+        else
+            heap = heap_swap_node(rchild,self, heap);
+            heap = heap_down_sink(rchild, heap);
+        end
+    end
+elseif(lchild <= heap.size)
+    if(heap.hvc(self) < heap.hvc(lchild))
+        heap = heap_swap_node(lchild,self, heap);
+    end
+end
+end
+
+function heap = heap_swap_node(node1,node2, heap)
+
+    t_index = heap.index(node1);
+    t_hvc = heap.hvc(node1);
+    
+    heap.index(node1) = heap.index(node2);
+    heap.hvc(node1) = heap.hvc(node2);
+
+    heap.index(node2) = t_index;
+    heap.hvc(node2) = t_hvc;
+
+end
+
+function heap = min_heap_popup(heap)
+heap = heap_swap_node(1,heap.size, heap);
+heap.size =heap.size -1;
+heap = heap_down_sink(1,heap);
+end
+
